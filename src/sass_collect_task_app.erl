60ed71a65ff757931082f736d10b146179404fab
edward@sqor.com <edward@sqor.com>
2014-06-15 10:18:15 -0700

    skeltoh compile works

--- src/sass_collect_task_app.erl
+++ src/sass_collect_task_app.erl
@@ -1,16 +1,37 @@
 -module(sass_collect_task_app).
-
 -behaviour(application).
 
+-define(APPS, [kernel,
+              stdlib,
+              inets,
+              lager,
+              ranch,
+              cowlib,
+              cowboy,
+              jsx,
+              msgpack,
+              sqor_logger,
+              resterl, 
+              resource_discovery]).
+
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/0, start/2, stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
+
+start() ->
+   [begin start_app(A) end || A <- ?APPS].
+
 
 start(_StartType, _StartArgs) ->
+    
     sass_collect_task_sup:start_link().
 
 stop(_State) ->
     ok.
+
+start_app(A)->
+    application:start(A),
+    io:format("~p started~n ",[A]).
